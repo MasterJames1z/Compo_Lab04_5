4d30ea1850e1440edab3fd59dec7a816c917c192
Worawut Aunkham <jameskater350@gmail.com>
2022-08-03 23:27:44 +0700

    5.2 show list of passengers

---
+++ lab04_5/src/services/EventService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL:
+    "https://my-json-server.typicode.com/se331-2022/passengerdb/passenger?_page=1&_limit=5",
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export default {
+  getPassengers(perPage, page) {
+    return apiClient.get("/passenger?_page=" + page + " &_limit=" + perPage);
+  },
+  //Added new call
+  getPassenger(id) {
+    return apiClient.get("/passenger/" + id);
+  },
+};
